1d399a443e59f3bf34ee5cdf6695610a9e5b68a0
Administrator <jonathan.taylor.programmer@gmail.com>
2019-10-25 12:29:18 +0100

    Potter - rename things to match Python conventions

--- potter/potter.py
+++ potter/potter.py
@@ -1,45 +1,50 @@
 from collections import defaultdict
 
 PC_DISCOUNTS = defaultdict(lambda: 0, {
-  2: 5,
-  3: 10,
-  4: 20,
-  5: 25
+    2: 5,
+    3: 10,
+    4: 20,
+    5: 25
 })
 
-def filterDict(d, p):
-  return {k: v for k, v in d.items() if p(k, v)}
 
-def removeBooks(groupedBooks, setOfBooks):
-  for book in setOfBooks:
-    groupedBooks[book].pop()
-  return filterDict(groupedBooks, lambda _, v: v)
+def filter_dict(d, p):
+    return {k: v for k, v in d.items() if p(k, v)}
 
-def priceFor(setOfBooks):
-  numBooks = len(setOfBooks)
-  fullPrice = numBooks * 8
-  pc_discount = PC_DISCOUNTS[numBooks]
-  discount = fullPrice / 100 * pc_discount
-  return fullPrice - discount
 
-def chooseSetOfBooks(groupedBooks):
-  lengths = [len(v) for v in groupedBooks.values()]
-  lengths.sort()
-  if lengths != [1, 1, 2, 2, 2]:
-    return groupedBooks.keys()
-  lengthOneItems = filterDict(groupedBooks, lambda _, v: len(v) == 1)
-  lengthTwoItems = filterDict(groupedBooks, lambda _, v: len(v) == 2)
-  lengthOneKeys = list(lengthOneItems.keys())
-  lengthTwoKeys = list(lengthTwoItems.keys())
-  return lengthOneKeys[:1] + lengthTwoKeys
+def remove_books(grouped_books, set_of_books):
+    for book in set_of_books:
+        grouped_books[book].pop()
+    return filter_dict(grouped_books, lambda _, v: v)
+
+
+def price_for(set_of_books):
+    num_books = len(set_of_books)
+    full_price = num_books * 8
+    pc_discount = PC_DISCOUNTS[num_books]
+    discount = full_price / 100 * pc_discount
+    return full_price - discount
+
+
+def choose_books(grouped_books):
+    lengths = [len(v) for v in grouped_books.values()]
+    lengths.sort()
+    if lengths != [1, 1, 2, 2, 2]:
+        return grouped_books.keys()
+    length_one_items = filter_dict(grouped_books, lambda _, v: len(v) == 1)
+    length_two_items = filter_dict(grouped_books, lambda _, v: len(v) == 2)
+    length_one_keys = list(length_one_items.keys())
+    length_two_keys = list(length_two_items.keys())
+    return length_one_keys[:1] + length_two_keys
+
 
 def potter(books):
-  groupedBooks = defaultdict(list)
-  for book in books:
-    groupedBooks[book].append(book)
-  total = 0
-  while groupedBooks:
-    setOfBooks = chooseSetOfBooks(groupedBooks)
-    total = total + priceFor(setOfBooks)
-    groupedBooks = removeBooks(groupedBooks, setOfBooks)
-  return total
+    grouped_books = defaultdict(list)
+    for book in books:
+        grouped_books[book].append(book)
+    total = 0
+    while grouped_books:
+        set_of_books = choose_books(grouped_books)
+        total = total + price_for(set_of_books)
+        grouped_books = remove_books(grouped_books, set_of_books)
+    return total
