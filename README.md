093f91078ce40e3ff6aeb6e443f7a6ecadef38ce
Administrator <jonathan.taylor.programmer@gmail.com>
2019-10-30 13:54:12 +0000

    AoC 2015 Day 5 (part 2)

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Also, attempts at a few _katas_ and _Advent of Code_ problems.
 
 * [_Classic Computer Science Problems in Python_](https://www.manning.com/books/classic-computer-science-problems-in-python)
 * [KataCatalogue](http://codingdojo.org/KataCatalogue/)
-* [Advent of Code](https://adventofcode.com/)
+* [Advent of Code](https://adventofcode.com/events)
--- aoc/2015/Day05/main.py
+++ aoc/2015/Day05/main.py
@@ -1,38 +1,66 @@
 import itertools
 
-vowels = "aeiou"
+
+def part1_check_1(s):
+    return len([c for c in s if c in "aeiou"]) >= 3
 
 
-def has_three_vowels(s):
-    return len([c for c in s if c in vowels]) >= 3
-
-
-def has_repeated_letter(s):
+def part1_check_2(s):
     grouped = itertools.groupby(s)
     lengths = [len(list(v)) for _, v in grouped]
     return [length > 1 for length in lengths].count(True) > 0
 
 
-def has_no_disallowed_pairs(s):
+def part1_check_3(s):
     disallowed_pairs = ["ab", "cd", "pq", "xy"]
     return len([dp for dp in disallowed_pairs if dp in s]) == 0
 
 
-def passes_all_checks(s):
+def passes_all_part1_checks(s):
     fns = [
-        has_three_vowels,
-        has_repeated_letter,
-        has_no_disallowed_pairs
+        part1_check_1,
+        part1_check_2,
+        part1_check_3
+    ]
+    return all([fn(s) for fn in fns])
+
+
+def part2_check_1(s):
+    for pos in range(len(s)):
+        pair = s[pos:pos + 2]
+        if pair in s[pos + 2:]: return True
+    return False
+
+
+def part2_check_2(s):
+    for pos in range(len(s)):
+        letters = s[pos:pos + 3]
+        if len(letters) == 3:
+            [a, _, b] = letters
+            if a == b: return True
+    return False
+
+
+def passes_all_part2_checks(s):
+    fns = [
+        part2_check_1,
+        part2_check_2
     ]
     return all([fn(s) for fn in fns])
 
 
 def part1(ss):
-    answer = [passes_all_checks(s) for s in ss].count(True)
+    answer = [passes_all_part1_checks(s) for s in ss].count(True)
     print(f"part 1 answer: {answer}")
+
+
+def part2(ss):
+    answer = [passes_all_part2_checks(s) for s in ss].count(True)
+    print(f"part 2 answer: {answer}")
 
 
 if __name__ == "__main__":
     with open("aoc/2015/Day05/input.txt") as f:
         strings = f.readlines()
         part1(strings)
+        part2(strings)
