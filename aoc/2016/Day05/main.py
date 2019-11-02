eba0dd615a64588186f92dfd958b2f2cd3403a1e
Administrator <jonathan.taylor.programmer@gmail.com>
2019-11-02 13:13:42 +0000

    AoC 2016 Day 5 (improvement)

--- aoc/2016/Day05/main.py
+++ aoc/2016/Day05/main.py
@@ -18,39 +18,30 @@ def find_next_00000(number):
     return md5, number
 
 
-def find_password(fn):
-    number = 0
-    chars = list("????????")
-    index = 0
-    while chars.count("?") > 0:
-        md5, number = find_next_00000(number)
-        char, pos = fn(md5, index)
-        print(md5, number, char, pos)
-        if pos is not None and chars[pos] == "?":
-            chars[pos] = char
-            index = index + 1
-        number = number + 1
-    return "".join(chars)
-
-
 def part1():
-    def fn(md5, index):
-        return md5[5], index
-
-    password = find_password(fn)
+    number = 0
+    password = ""
+    for _ in range(8):
+        md5, number = find_next_00000(number)
+        char = md5[5]
+        print(f"number: {number}; md5: {md5}; char: {char}")
+        password = password + char
+        number = number + 1
     print(f"part 1 answer: {password}")
 
 
 def part2():
-    def fn(md5, _):
+    number = 0
+    pos_char_map = dict()
+    while len(pos_char_map) < 8:
+        md5, number = find_next_00000(number)
         pos = int(md5[5], 16)
-        char = md5[6]
-        if pos < 8:
-            return char, pos
-        else:
-            return char, None
-
-    password = find_password(fn)
+        if pos < 8 and pos not in pos_char_map:
+            char = md5[6]
+            print(f"number: {number}; md5: {md5}; pos: {pos}; char: {char}")
+            pos_char_map[pos] = char
+        number = number + 1
+    password = "".join([pos_char_map[pos] for pos in sorted(pos_char_map)])
     print(f"part 2 answer: {password}")
 
 
