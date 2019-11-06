82c23bb2f54172d5c5f779e11185d0925e84ae4c
Administrator <jonathan.taylor.programmer@gmail.com>
2019-11-06 12:48:44 +0000

    AoC 2018 Day 5 (part 2)

--- aoc/2018/Day05/main.py
+++ aoc/2018/Day05/main.py
@@ -1,3 +1,6 @@
+import re
+
+
 def find_reacting_units(polymer):
     for pos in range(len(polymer) - 1):
         code1 = ord(polymer[pos])
@@ -6,16 +9,33 @@ def find_reacting_units(polymer):
     return None
 
 
-def part1(polymer):
+def react_polymer(polymer):
     while True:
         pos = find_reacting_units(polymer)
-        print(f"pos: {pos}")
         if pos is None: break
         polymer = polymer[:pos] + polymer[pos + 2:]
-    print(f"part 1 answer: {len(polymer)}")
+    return polymer
+
+
+def part1(polymer):
+    shortest_polymer = react_polymer(polymer)
+    print(f"part 1 answer: {len(shortest_polymer)}")
+
+
+def make_new_polymer(polymer, letter):
+    return re.sub(letter, "", polymer, flags=re.IGNORECASE)
+
+
+def part2(polymer):
+    letters = set(str.lower(polymer))
+    new_polymers = [make_new_polymer(polymer, l) for l in letters]
+    reacted_lengths = [len(react_polymer(np)) for np in new_polymers]
+    sorted_lengths = sorted(reacted_lengths)
+    print(f"part 2 answer: {sorted_lengths[0]}")
 
 
 if __name__ == "__main__":
     with open("aoc/2018/Day05/input.txt") as f:
         polymer = f.read().rstrip()
         part1(polymer)
+        part2(polymer)
