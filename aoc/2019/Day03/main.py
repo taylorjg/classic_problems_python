f2fc2814e4bd44c0b9b2d2bc53a9348def90f3b8
Administrator <jonathan.taylor.programmer@gmail.com>
2019-12-03 14:17:18 +0000

    AoC 2019 Day 3 (part 2)

--- aoc/2019/Day03/main.py
+++ aoc/2019/Day03/main.py
@@ -8,8 +8,8 @@ def advance(pos, direction, distance):
 
 
 def find_path(wire):
-    path = []
     pos = 0, 0
+    path = [pos]
     for instruction in wire:
         direction = instruction[0]
         distance = int(instruction[1:])
@@ -20,8 +20,8 @@ def find_path(wire):
 
 
 def find_crossings(path1, path2):
-    s1 = set(path1)
-    s2 = set(path2)
+    s1 = set(path1[1:])
+    s2 = set(path2[1:])
     return s1.intersection(s2)
 
 
@@ -30,27 +30,39 @@ def manhattan_distance(pos):
     return abs(x) + abs(y)
 
 
-def find_nearest_crossing(crossings):
-    return sorted([manhattan_distance(crossing) for crossing in crossings])[0]
+def count_steps(path, pos):
+    count = 0
+    while True:
+        if path[count] == pos:
+            break
+        count = count + 1
+    return count
 
 
-def part1(wire1, wire2):
-    path1 = find_path(wire1)
-    path2 = find_path(wire2)
-    crossings = find_crossings(path1, path2)
-    answer = find_nearest_crossing(crossings)
+def combined_steps(path1, path2, crossing):
+    steps1 = count_steps(path1, crossing)
+    steps2 = count_steps(path2, crossing)
+    return steps1 + steps2
+
+
+def part1(crossings):
+    answer = sorted([manhattan_distance(crossing) for crossing in crossings])[0]
     print(f"part 1 answer: {answer}")
 
 
+def part2(path1, path2, crossings):
+    answer = sorted([combined_steps(path1, path2, crossing) for crossing in crossings])[0]
+    print(f"part 2 answer: {answer}")
+
+
 if __name__ == "__main__":
-    # line1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
-    # line2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
-    # wire1 = line1.rstrip().split(',')
-    # wire2 = line2.rstrip().split(',')
-    # part1(wire1, wire2)
     with open("aoc/2019/Day03/input.txt") as f:
         line1 = f.readline()
         line2 = f.readline()
         wire1 = line1.rstrip().split(',')
         wire2 = line2.rstrip().split(',')
-        part1(wire1, wire2)
+        path1 = find_path(wire1)
+        path2 = find_path(wire2)
+        crossings = find_crossings(path1, path2)
+        part1(crossings)
+        part2(path1, path2, crossings)
