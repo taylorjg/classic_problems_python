1bf5f4f0f46474582045b6b7c522430901afee70
Administrator <jonathan.taylor.programmer@gmail.com>
2019-12-05 15:41:21 +0000

    AoC 2019 Day 5 (part 1 - refactoring)

--- aoc/2019/Day05/main.py
+++ aoc/2019/Day05/main.py
@@ -1,46 +1,70 @@
+ADD = 1
+MULTIPLY = 2
+SAVE = 3
+OUTPUT = 4
+HALT = 99
+
+
+# TODO: add a dictionary of opcode to function to execute
+
+def decode_instruction(instruction):
+    instruction_string = str(instruction)
+    opcode = int(instruction_string[-2:])
+    modes_string = instruction_string[:-2]
+    left_padded_modes_string = modes_string.rjust(2, '0')
+    reversed_modes_string = left_padded_modes_string[::-1]
+    modes = [int(c) for c in reversed_modes_string]
+    return opcode, modes
+
+
+def execute_arithmetic_instruction(program, pos, modes, op):
+    param1 = program[pos + 1]
+    param2 = program[pos + 2]
+    param3 = program[pos + 3]
+    a = param1 if modes[0] else program[param1]
+    b = param2 if modes[1] else program[param2]
+    c = op(a, b)
+    program[param3] = c
+    return pos + 4
+
+
+def execute_save_instruction(program, pos, input):
+    param1 = program[pos + 1]
+    program[param1] = input
+    return pos + 2
+
+
+def execute_output_instruction(program, pos, modes, outputs):
+    param1 = program[pos + 1]
+    output = param1 if modes[0] else program[param1]
+    outputs.append(output)
+    return pos + 2
+
+
 def run_program(program, input):
     outputs = []
     pos = 0
     while True:
-        instruction = str(program[pos])
-        opcode = int(instruction[-2:])
-        raw_modes = instruction[:-2]
-        padded_raw_modes = raw_modes.rjust(3, '0')
-        [modea, modeb, _] = [int(c) for c in padded_raw_modes[::-1]]
-        if opcode == 99:  # halt
+        instruction = program[pos]
+        opcode, modes = decode_instruction(instruction)
+        if opcode == ADD:
+            pos = execute_arithmetic_instruction(program, pos, modes, lambda x, y: x + y)
+        if opcode == MULTIPLY:
+            pos = execute_arithmetic_instruction(program, pos, modes, lambda x, y: x * y)
+        if opcode == SAVE:
+            pos = execute_save_instruction(program, pos, input)
+        if opcode == OUTPUT:
+            pos = execute_output_instruction(program, pos, modes, outputs)
+        if opcode == HALT:
             break
-        if opcode == 1:  # add
-            param1 = program[pos + 1]
-            param2 = program[pos + 2]
-            param3 = program[pos + 3]
-            a = param1 if modea else program[param1]
-            b = param2 if modeb else program[param2]
-            program[param3] = a + b
-            pos = pos + 4
-        if opcode == 2:  # multiply
-            param1 = program[pos + 1]
-            param2 = program[pos + 2]
-            param3 = program[pos + 3]
-            a = param1 if modea else program[param1]
-            b = param2 if modeb else program[param2]
-            program[param3] = a * b
-            pos = pos + 4
-        if opcode == 3:  # save
-            param1 = program[pos + 1]
-            program[param1] = input
-            pos = pos + 2
-        if opcode == 4:  # output
-            param1 = program[pos + 1]
-            output = param1 if modea else program[param1]
-            outputs.append(output)
-            print(output)
-            pos = pos + 2
     return outputs
 
 
 def part1(program):
-    output = run_program(program, 1)
-    print(f"part 1 answer: {output}")
+    outputs = run_program(program, 1)
+    print(outputs)
+    answer = outputs[-1]
+    print(f"part 1 answer: {answer}")
 
 
 if __name__ == "__main__":
