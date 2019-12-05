418aedbfb3d4c1b589d63202f6e033d25f693a49
Administrator <jonathan.taylor.programmer@gmail.com>
2019-12-05 16:00:22 +0000

    AoC 2019 Day 5 (part 1 - more refactoring)

--- aoc/2019/Day05/main.py
+++ aoc/2019/Day05/main.py
@@ -1,15 +1,18 @@
-ADD = 1
-MULTIPLY = 2
-SAVE = 3
-OUTPUT = 4
-HALT = 99
+from functools import partial
+from enum import Enum
 
 
-# TODO: add a dictionary of opcode to function to execute
+class Opcodes(Enum):
+    ADD = 1
+    MULTIPLY = 2
+    SAVE = 3
+    OUTPUT = 4
+    HALT = 99
+
 
 def decode_instruction(instruction):
     instruction_string = str(instruction)
-    opcode = int(instruction_string[-2:])
+    opcode = Opcodes(int(instruction_string[-2:]))
     modes_string = instruction_string[:-2]
     left_padded_modes_string = modes_string.rjust(2, '0')
     reversed_modes_string = left_padded_modes_string[::-1]
@@ -17,7 +20,7 @@ def decode_instruction(instruction):
     return opcode, modes
 
 
-def execute_arithmetic_instruction(program, pos, modes, op):
+def execute_arithmetic_instruction(op, program, pos, modes):
     param1 = program[pos + 1]
     param2 = program[pos + 2]
     param3 = program[pos + 3]
@@ -28,35 +31,39 @@ def execute_arithmetic_instruction(program, pos, modes, op):
     return pos + 4
 
 
-def execute_save_instruction(program, pos, input):
+def execute_save_instruction(input, program, pos, _):
     param1 = program[pos + 1]
     program[param1] = input
     return pos + 2
 
 
-def execute_output_instruction(program, pos, modes, outputs):
+def execute_output_instruction(outputs, program, pos, modes):
     param1 = program[pos + 1]
     output = param1 if modes[0] else program[param1]
     outputs.append(output)
     return pos + 2
 
 
+def make_intcode_dict(input, outputs):
+    return {
+        Opcodes.ADD: partial(execute_arithmetic_instruction, lambda x, y: x + y),
+        Opcodes.MULTIPLY: partial(execute_arithmetic_instruction, lambda x, y: x * y),
+        Opcodes.SAVE: partial(execute_save_instruction, input),
+        Opcodes.OUTPUT: partial(execute_output_instruction, outputs)
+    }
+
+
 def run_program(program, input):
     outputs = []
+    intcode_dict = make_intcode_dict(input, outputs)
     pos = 0
     while True:
         instruction = program[pos]
         opcode, modes = decode_instruction(instruction)
-        if opcode == ADD:
-            pos = execute_arithmetic_instruction(program, pos, modes, lambda x, y: x + y)
-        if opcode == MULTIPLY:
-            pos = execute_arithmetic_instruction(program, pos, modes, lambda x, y: x * y)
-        if opcode == SAVE:
-            pos = execute_save_instruction(program, pos, input)
-        if opcode == OUTPUT:
-            pos = execute_output_instruction(program, pos, modes, outputs)
-        if opcode == HALT:
+        if opcode == Opcodes.HALT:
             break
+        fn = intcode_dict[opcode]
+        pos = fn(program, pos, modes)
     return outputs
 
 
