e9687e9d070c96ffb5f8f409d8036ed1e63c72cf
Administrator <jonathan.taylor.programmer@gmail.com>
2019-12-13 15:42:59 +0000

    AoC 2019 Day 13 (part 2 - main game loop)

--- aoc/2019/Day13/main.py
+++ aoc/2019/Day13/main.py
@@ -134,6 +134,20 @@ def run_program(program, input):
     return outputs
 
 
+def run_program_until_next_instruction(program, input):
+    outputs = []
+    intcode_dict = make_intcode_dict(input, outputs)
+    while True:
+        instruction = program.get_current_instruction()
+        opcode, modes = decode_instruction(instruction)
+        if opcode == Opcodes.HALT:
+            return None
+        fn = intcode_dict[opcode]
+        program.pos = fn(program, modes)
+        if len(outputs) == 3:
+            return outputs
+
+
 def split_list_every(xs, n):
     num_layers = len(xs) // n
     for idx in range(num_layers):
@@ -142,13 +156,86 @@ def split_list_every(xs, n):
         yield xs[start_idx:end_idx]
 
 
+SCREEN_WIDTH = 40
+SCREEN_HEIGHT = 24
+
+EMPTY_TILE_ID = 0
+WALL_TILE_ID = 1
+BLOCK_TILE_ID = 2
+PADDLE_TILE_ID = 3
+BALL_TILE_ID = 4
+
+EMPTY_CHAR = " "
+WALL_CHAR = "#"
+BLOCK_CHAR = "."
+PADDLE_CHAR = "-"
+BALL_CHAR = "o"
+
+SCREEN_CHARS = {
+    EMPTY_TILE_ID: EMPTY_CHAR,
+    WALL_TILE_ID: WALL_CHAR,
+    BLOCK_TILE_ID: BLOCK_CHAR,
+    PADDLE_TILE_ID: PADDLE_CHAR,
+    BALL_TILE_ID: BALL_CHAR
+}
+
+
+def read_initial_screen(program):
+    screen = {}
+    paddle_pos = None
+    ball_pos = None
+    for _ in range(SCREEN_WIDTH * SCREEN_HEIGHT):
+        [x, y, tile_id] = run_program_until_next_instruction(program, 0)
+        screen[x, y] = SCREEN_CHARS[tile_id]
+        if tile_id == PADDLE_TILE_ID:
+            paddle_pos = x, y
+        if tile_id == BALL_TILE_ID:
+            ball_pos = x, y
+    return screen, paddle_pos, ball_pos
+
+
+def draw_screen(screen):
+    for y in range(SCREEN_HEIGHT):
+        row = [screen[x, y] for x in range(SCREEN_WIDTH)]
+        print("".join(row))
+    print()
+
+
 def part1(values):
     program = Program(values)
     outputs = run_program(program, 0)
-    game_instructions = split_list_every(outputs, 3)
-    tile_ids = [tile_id for [_, _, tile_id] in game_instructions]
+    instructions = list(split_list_every(outputs, 3))
+    tile_ids = [tile_id for [_, _, tile_id] in instructions]
     answer = tile_ids.count(2)
     print(f"part 1 answer: {answer}")
+
+
+def part2(values):
+    values[0] = 2
+    program = Program(values)
+    screen, paddle_pos, ball_pos = read_initial_screen(program)
+    current_score = None
+    joystick_tilt = 0
+    draw_screen(screen)
+    while True:
+        instruction = run_program_until_next_instruction(program, joystick_tilt)
+        if instruction is None: break
+        [x, y, tile_id] = instruction
+        if x == -1 and y == 0:
+            current_score = tile_id
+            continue
+        screen[x, y] = SCREEN_CHARS[tile_id]
+        if (x, y) == paddle_pos and tile_id == EMPTY_TILE_ID:
+            continue
+        if (x, y) == ball_pos and tile_id == EMPTY_TILE_ID:
+            continue
+        if tile_id == PADDLE_TILE_ID:
+            paddle_pos = x, y
+        if tile_id == BALL_TILE_ID:
+            ball_pos = x, y
+        draw_screen(screen)
+        # TODO: keep the ball in the air by adjusting the joystick tilt
+    print(f"part 2 answer: {current_score}")
 
 
 if __name__ == "__main__":
@@ -156,3 +243,4 @@ if __name__ == "__main__":
         line = f.read()
         values = [int(s) for s in line.split(',')]
         part1(values)
+        part2(values)
